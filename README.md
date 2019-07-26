e962f8efecbf1c28a1d69f1bac20eb3303cba97b
Kevin Grube <grube.kevin@gmail.com>
2019-07-25 19:42:01 -0700

    Add additional authentication methods

--- README.md
+++ README.md
@@ -1,16 +1,79 @@
 # node-itglue
 
+Simple authentication bindings for ITGlue.
+
+## Authentication
+
+This library offers three methods of authentication: api key, bearer and user.
+
+### API Key
+
 ```js
 
 const ITGlue = require('node-itglue');
 
 const itg = new ITGlue({
-  apikey: '// your api key',
+  // API Key auth
+  // your api key
+  apikey: 'abcdef12345',
  
   // optional
-  eu: false, // set true if you're connecting to the European datacenter
-  timeout: 10000, // timeout in ms
-   
+  // set true if you're connecting to the European datacenter with API Key auth
+  eu: false, 
+  // timeout in ms
+  timeout: 10000, 
+  
+  // set the mode for another authentication mode
+  // one of ['apikey', 'user', 'bearer']
+  mode: 'apikey'
+  // required when mode is set to 'user'
+  companyUrl: 'https://yourcompany.itglue.com'
+  // required when mode is set to 'user'
+  user: { email: 'you@yourcompany.com', password: 'yourpassword'}
+  // JWT, required when mode is set to bearer
+  token: '12345abcdef' 
+})
+
+
+itg.get({path, params})
+  .then(result => {
+    // result
+  })
+  .catch(error => {
+    // error
+  });
+  
+itg.post({path, params, body});
+
+itg.delete({path, params});
+
+itg.patch({path, params, body});
+
+```
+
+### Bearer Authentication
+
+```js
+const itg = new ITGlue({
+  mode: 'bearer',
+  token: '',
 })
 
 ```
+
+### User Authentication
+
+The library will request and cache a new bearer token in memory for the duration of the running process.
+
+```js
+const itg = new ITGlue({
+  mode: 'user',
+  companyUrl: 'https://yourcompany.itglue.com',
+  user: {
+    email: '', 
+    password: ''
+  }
+})
+
+```
+
--- src/index.js
+++ src/index.js
@@ -35,17 +35,41 @@ const DELETE = 'delete';
 
 const BASE_URL = 'https://api.itglue.com';
 const BASE_URL_EU = 'https://api.eu.itglue.com';
+const BASE_URL_MOBILE = 'https://api-mobile-prod.itglue.com/api';
+
+const MODE_BEARER = 'bearer';
+const MODE_USER = 'user';
+const MODE_APIKEY = 'apikey';
 
 /**
  *
  * @param config
- * @param config.apikey
+ * @param [config.mode=apikey] ['apikey', 'bearer', 'user']
+ * @param [config.apikey] specify to access API methods
  * @param [config.timeout=10000]
  * @param [config.eu=false]
+ * @param [config.companyUrl] specify to get a bearer token
+ * @param [config.token] specify authentication using a bearer token
+ * @param [config.user]
+ * @param [config.user.email]
+ * @param [config.user.password]
+ * @param {String} [config.user.otp] one time password for MFA -- must be a string
  * @returns {Promise<{}>}
  * @constructor
  */
-function ITGlue({apikey, timeout, eu}) {
+function ITGlue({
+  apikey, mode = MODE_APIKEY, timeout, eu, companyUrl, token, user: {email, password, otp} = {},
+}) {
+  if (!companyUrl && mode === MODE_USER) {
+    throw `companyUrl must be defined in mode ${MODE_USER}`;
+  }
+  if (!token && mode === MODE_BEARER) {
+    throw `token must be defined in mode ${MODE_BEARER}`;
+  }
+  if (!apikey && mode === MODE_APIKEY) {
+    throw `apikey must be defined in mode ${MODE_APIKEY}`;
+  }
+
   if (!timeout) {
     timeout = 10000;
   }
@@ -54,12 +78,34 @@ function ITGlue({apikey, timeout, eu}) {
     baseURL = BASE_URL_EU;
   }
 
-  this.config = {
-    baseURL,
-    headers: {
+  this.config = {};
+  this.mode = mode;
+  this.token = token;
+  this.user = {email, password, otp};
+  this.apikey = apikey;
+  this.companyUrl = companyUrl;
+
+  if (mode === MODE_USER) {
+    this.config.baseURL = companyUrl;
+    this.config.headers = {
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+    };
+  } else if (mode === MODE_BEARER) {
+    this.config.baseURL = BASE_URL_MOBILE;
+    this.config.headers = {
+      'cache-control': 'no-cache',
+      'content-type': 'application/vnd.api+json',
+      'authorization': `Bearer ${token}`,
+    };
+  } else {
+    this.config.baseURL = baseURL;
+    this.config.headers = {
       'x-api-key': apikey,
-    },
-  };
+      'cache-control': 'no-cache',
+      'content-type': 'application/vnd.api+json',
+    };
+  }
 }
 
 /**
@@ -72,7 +118,7 @@ function ITGlue({apikey, timeout, eu}) {
  */
 ITGlue.prototype.client = function ({path, params, body, method}) {
   return new Promise((resolve, reject) => {
-    return axios(path, {...this.config, method, params, data: body, url: path})
+    return axios(path, {...this.config, method, params, data: body})
       .then(res => {
         return resolve(res);
       })
@@ -83,7 +129,7 @@ ITGlue.prototype.client = function ({path, params, body, method}) {
           data = err.response.data;
         }
 
-        if (data.errors) {
+        if (data && data.errors) {
           return reject(data.errors);
         } else if (err.message) {
           return reject(err.message);
@@ -92,20 +138,114 @@ ITGlue.prototype.client = function ({path, params, body, method}) {
   });
 };
 
+ITGlue.prototype.handler = function (options) {
+  if (this.mode === MODE_APIKEY) {
+    return this.client(options);
+  }
+  if (this.mode === MODE_USER && !this.token) {
+    return this.getItGlueJsonWebToken(this.user)
+      .then(token => {
+        this.token = token;
+        this.setAuthenticationMode(MODE_BEARER);
+        return this.client(options);
+      });
+  } else if (this.mode === MODE_USER && this.token) {
+    this.setAuthenticationMode(MODE_BEARER);
+    return this.client(options);
+  }
+  if (this.mode === MODE_BEARER) {
+    return this.client(options);
+  }
+};
+
 ITGlue.prototype.get = function ({path, params}) {
-  return this.client({method: GET, path, params});
+  return this.handler({method: GET, path, params});
 };
 
 ITGlue.prototype.post = function ({path, params, body}) {
-  return this.client({method: POST, path, params, body});
+  return this.handler({method: POST, path, params, body});
 };
 
 ITGlue.prototype.patch = function ({path, params, body}) {
-  return this.client({method: PATCH, path, params, body});
+  return this.handler({method: PATCH, path, params, body});
 };
 
 ITGlue.prototype.delete = function ({path, params}) {
-  return this.client({method: DELETE, path, params});
+  return this.handler({method: DELETE, path, params});
+};
+
+/**
+ *
+ * @param [mode] ['apikey', 'bearer', 'user']
+ */
+ITGlue.prototype.setAuthenticationMode = function (mode = 'apikey') {
+  this.mode = mode;
+  const {apikey, companyUrl, token} = this;
+  if (mode === MODE_USER) {
+    this.config.baseURL = companyUrl;
+    this.config.headers = {
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+    };
+  } else if (mode === MODE_BEARER) {
+    this.config.baseURL = BASE_URL_MOBILE;
+    this.config.headers = {
+      'cache-control': 'no-cache',
+      'content-type': 'application/vnd.api+json',
+      'authorization': `Bearer ${token}`,
+    };
+  } else {
+    this.config.baseURL = baseURL;
+    this.config.headers = {
+      'x-api-key': apikey,
+      'cache-control': 'no-cache',
+      'content-type': 'application/vnd.api+json',
+    };
+  }
+};
+
+/**
+ * @param email
+ * @param password
+ * @param otp
+ * @returns {Promise<{String}>}
+ * @constructor
+ */
+ITGlue.prototype.getItGlueJsonWebToken = function ({email, password, otp}) {
+  return this.client({
+    method: POST,
+    path: '/login',
+    params: {generate_jwt: 1, sso_disabled: 1},
+    body: {
+      user: {email, password, otp_attempt: otp},
+    },
+  })
+    .then(result => {
+      return result.data.token;
+    })
+    .then(token => {
+      return this.refreshItGlueJsonWebToken({token});
+    })
+    .then(token => {
+      return token;
+    });
+};
+
+/**
+ * @param token
+ * @returns {Promise<{String}>}
+ */
+ITGlue.prototype.refreshItGlueJsonWebToken = function ({token}) {
+  return this.client({
+    method: GET,
+    path: '/jwt/token',
+    params: {
+      refresh_token: token,
+    },
+  })
+    .then(result => {
+      return result.data.token;
+    });
 };
 
 
